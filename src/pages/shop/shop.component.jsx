ebcbfae6bf5892a9eaea6f1c05a384063e3cbd5e
ainunsn <ainun.s.n@gmail.com>
2020-06-15 10:07:46 +0700

    update shop pages

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {selectShopCollections} from '../../redux/shop/shop.selector';
-import CollectionsPreview from '../../components/preview-collections/collections-preview.component';
-
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 const ShopPage = ({collections}) =>  (
 	<div className='shop-page'>
-		{
-			collections.map(({id, ...otherCollectionProps}) => (
-				<CollectionsPreview key={id} {...otherCollectionProps}/>
-			))
-		}
+		<CollectionsOverview />
 	</div>
 );
 
-const mapStateToProps = createStructuredSelector({
-	collections: selectShopCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
